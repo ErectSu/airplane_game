f8be7b0f18edce3c659bfb1e176af4464183a5a8
ErectSu <2469070295@qq.com>
2018-09-01 23:02:18 +0800

    update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,88 @@ var Main = {
         Util.init()
     },
     start:function () {
-        selfPlane.init()
+        selfPlane.init();
+        this._initEvent();
+        this._render();
+    },
+
+    _render_t:null,
+    _render:function(){
+        this._render_t=setInterval(function(){
+            if (selfPlane.left){
+                selfPlane.move(-Main.keyMove,0)
+            }
+            if (selfPlane.up){
+                selfPlane.move(0,-Main.keyMove)
+            }
+            if (selfPlane.right){
+                selfPlane.move(Main.keyMove,0)
+            }
+            if (selfPlane.down){
+                selfPlane.move(0,Main.keyMove)
+            }
+        },1000/15)
+    },
+
+    keyMove:18,
+    _initEvent:function () {
+        window.onkeydown = function (e) {
+            var keynum;
+            var left = 37, up = 38, right = 39, down = 40;
+
+            if (window.event) {// IE
+                keynum = e.keyCode
+            } else if (e.which) {// Netscape/Firefox/Opera
+                keynum = e.which
+            }
+
+
+            switch(keynum){
+                case left:
+                    selfPlane.left = true;
+                    break;
+                case up:
+                    selfPlane.up = true;
+                    break;
+                case right:
+                    selfPlane.right = true;
+                    break;
+                case down:
+                    selfPlane.down = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        window.onkeyup = function (e) {
+            var keynum;
+            var left = 37, up = 38, right = 39, down = 40;
+
+            if (window.event) {// IE
+                keynum = e.keyCode
+            } else if (e.which) {// Netscape/Firefox/Opera
+                keynum = e.which
+            }
+
+
+            switch(keynum){
+                case left:
+                    selfPlane.left = false;
+                    break;
+                case up:
+                    selfPlane.up = false;
+                    break;
+                case right:
+                    selfPlane.right = false;
+                    break;
+                case down:
+                    selfPlane.down = false;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
+
 }
--- js/spirit.js
+++ js/spirit.js
@@ -2,6 +2,7 @@ var selfPlane = {
     x : 0,
     y : 0,
     score :0,
+    e:null,
     blood:100,
     init:function () {
         this.x = 10;
@@ -12,7 +13,25 @@ var selfPlane = {
         Util.selfPlaneElement.style.left = this.x + "px";
         Util.selfPlaneElement.style.top=this.y+"px";
         Util.parentElement.appendChild(this.e);
-    }
+    },
+    move:function (moveX,moveY) {
+        var x = this.x + moveX;
+        var y=this.y+moveY;
 
+        if(x<0-this.e.width/2||x>Util.windowWidth-this.e.width/2){
+            return ;
+        }
+        if(y<0-this.e.height/2||y>Util.windowHeight-this.e.height/2){
+            return ;
+        }
+        this.x=x;
+        this.y=y;
 
+        this.e.style.left=this.x+"px";
+        this.e.style.top=this.y+"px";
+    },
+    up:false,
+    down:false,
+    left:false,
+    right:false,
 }
